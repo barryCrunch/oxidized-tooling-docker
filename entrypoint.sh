44ae1929b5d77b3f9f3b38c805f22ce1ada8784c
barryCrunch <mbarry@packetdriving.com>
2019-02-05 19:15:58 +0000

    Add SSH Copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 pwd
-python /oxidized-tooling/netboxpull.py
+#python /oxidized-tooling/netboxpull.py
 sleep 10s
-cp router.db /tmp/oxidized/
+#cp router.db /tmp/oxidized/
+echo $PRIVATE_KEY > /tmp/oxidized/id_rsa
+echo $PUBLIC_KEY > /tmp/oxidized/id_rsa.pub
 /bin/sh
